c79f34f6bad4dce75bf29c712ead482a8bd3d620
Fabián García Vázquez <fabiangarciav@gmail.com>
2022-09-18 02:08:35 -0500

    Functional version that include javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+const rateBoxPopup = document.querySelector('.rate-box'); 
+const thankYouBoxPopup = document.querySelector('.thank-you-box');
+const spanRateNumber = document.querySelector('#numberRateSelected');
+const submitButton = document.querySelector('.submit-button');
+
+let rateNumberSelect = 0;
+
+function rateIconSelectedColor(rateNumber) {
+    rateNumberSelect = rateNumber;
+    for (let i=1; i<6; i++) {
+        document.getElementById('rateIcon'+i).className = "";
+    }
+    document.getElementById('rateIcon'+rateNumber).className = "selected";
+}
+
+function showThankYouBox() {
+    spanRateNumber.innerText = rateNumberSelect;
+    rateBoxPopup.setAttribute('style','display:none');
+    thankYouBoxPopup.setAttribute('style','display:block');
+}
